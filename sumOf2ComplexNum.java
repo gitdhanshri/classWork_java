2c99ad037bf771b6ac40b0d15471ebb407c04f06
gitdhanshri <2022bit049@sggs.ac.in>
2024-03-18 20:08:18 +0530

    Addition of two complex Number

---
+++ sumOf2ComplexNum.java
@@ -0,0 +1,25 @@
+class sumOf2complex{
+	double real;
+	double imaginary;
+	sumOf2complex obj1;
+	sumOf2complex obj2;
+	sumOf2complex(int real,int img){
+		this.real=real;
+		this.imaginary=img;
+	}
+	String  sum(sumOf2complex obj1,sumOf2complex obj2){
+		this.obj1=obj1;
+		this.obj2=obj2;
+		double real_add=(obj1.real+obj2.real);
+		double img_add=(obj1.imaginary+obj2.imaginary);
+		String str=(real_add+"+"+img_add+"i");
+		return str;
+	}
+
+	public static void main(String args[]){
+		sumOf2complex c1 =new sumOf2complex(4,6);
+		sumOf2complex c2 =new sumOf2complex(2,-3);
+		System.out.println("Addition of two Complex Number-->"+c1.sum(c1,c2));
+		
+	}
+}

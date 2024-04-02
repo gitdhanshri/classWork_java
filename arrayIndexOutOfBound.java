95076e8367c749e132b1961fadbba97be6787699
gitdhanshri <2022bit049@sggs.ac.in>
2024-04-02 21:27:46 +0530

    Getting an Exception arrayIndexOutOfBound.java

---
+++ arrayIndexOutOfBound.java
@@ -0,0 +1,12 @@
+class ArrayIndex{
+	public static void main(String args[]){
+		System.out.println(args[o]);
+	}
+}
+/*arrayIndexOutOfBound.java:3: error: cannot find symbol
+		System.out.println(args[o]);
+		                        ^
+  symbol:   variable o
+  location: class ArrayIndex
+1 error
+*/
